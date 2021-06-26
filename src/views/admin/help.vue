905b5df5e2311454e8b89f3936ed6ec517f91598
DRC <caiwenzhe2021@gmail.com>
2021-06-26 20:37:24 +0800

    add Help.vue

---
+++ src/views/admin/help.vue
@@ -0,0 +1,19 @@
+<template>
+	<el-container>
+		<el-main>
+			<el-card class="box-card">
+				<div slot="header" class="clearfix">
+					<span>帮助</span>
+				</div>
+				<div class="text item">
+					本项目基于Vue.js 2.0开发，使用ElementUI、eCharts、BootStrapVue等开源组件。<br>
+					作者：DexterCai <br>
+				</div>
+			</el-card>
+		</el-main>
+	</el-container>
+</template>
+
+<script></script>
+
+<style></style>
